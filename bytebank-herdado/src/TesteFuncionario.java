76bb48d3a343a32092577b9419eb946fae064550
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-09-04 22:02:20 -0300

    add project bytebank-herdado

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,19 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("Nico Steppat");
+		nico.setCpf("33269858741");
+		nico.setSalario(2600.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getSalario());
+		System.out.println(nico.getBonificacao());
+		
+		
+
+	}
+
+}
